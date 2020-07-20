eb501274a02f7af9c07070ecd52909927f2eeab9
Carlosmilla <carlos.milla.torres@gmail.com>
2020-07-20 17:43:47 -0400

    springcrud
    spring
    crud
    mysql

---
+++ CrudSpringBoot/src/main/java/com/crud/demo/service/PersonaService.java
@@ -0,0 +1,45 @@
+package com.crud.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.crud.demo.interfaceServices.IpersonaService;
+import com.crud.demo.interfaces.Ipersona;
+import com.crud.demo.modelo.Persona;
+
+@Service
+public class PersonaService implements IpersonaService {
+
+	@Autowired
+	private Ipersona data;
+	
+	@Override
+	public List<Persona> listar() {
+		return (List<Persona>)data.findAll();
+	}
+
+	@Override
+	public Optional<Persona> listarid(int id) {
+		return data.findById(id);
+	}
+
+	@Override
+	public int save(Persona p) {
+		int res=0;
+		Persona persona=data.save(p);
+		if(!persona.equals(null)) {
+			res=1;
+		}
+		return res;
+	}
+
+	@Override
+	public void delete(int id) {
+		data.deleteById(id);
+	}
+	
+
+}
